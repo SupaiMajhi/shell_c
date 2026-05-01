22aee323c9908859264c331179158f6a833329c0
Supai Majhi <sameertudu978@gmail.com>
2026-05-01 20:06:44 +0530

    feat: add REPL flow

--- src/main.c
+++ src/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
   char command[1024];
   // Flush after every printf
   setbuf(stdout, NULL);
-
-  printf("$ ");
-  fgets(command, sizeof(command), stdin);
-  command[strlen(command) - 1] = '\0';
-  printf("%s: command not found", command);
+  while(true) {
+    printf("$ ");
+    fgets(command, sizeof(command), stdin);
+    command[strlen(command) - 1] = '\0';
+    printf("%s: command not found\n", command);
+  }
   return 0;
 }
