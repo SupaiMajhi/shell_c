8bbf502e0f24d44f7fcdb8760fcaeada6c5f8df5
Supai Majhi <sameertudu978@gmail.com>
2026-05-01 20:03:48 +0530

    feat: add command not found

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
+  char command[1024];
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
-
+  fgets(command, sizeof(command), stdin);
+  command[strlen(command) - 1] = '\0';
+  printf("%s: command not found", command);
   return 0;
 }
