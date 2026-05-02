0a8d1f69a8e643be8f1df4eac43fe2da0dfebdf5
Supai Majhi <sameertudu978@gmail.com>
2026-05-02 21:00:40 +0530

    add type command

--- src/main.c
+++ src/main.c
@@ -14,10 +14,17 @@ int main(int argc, char *argv[]) {
     fgets(command, sizeof(command), stdin);
     command[strlen(command) - 1] = '\0';
     
-    if(strcmp(command, "exit") == 0){
+    if(strcmp(command, "exit") == 0) {
       break;
     } else if(strncmp(command, "echo ", 5) == 0) {
       printf("%s\n", command + 5);
+    } else if(strncmp(command, "type ", 5) == 0) {
+      char* sub = command + 5;
+      if(strcmp(sub, "echo") == 0 || strcmp(sub, "exit") == 0 || strcmp(sub, "type") == 0){
+        printf("%s is a shell builtin\n", sub);
+      } else {
+        printf("%s: not found\n", sub);
+      }
     } else {
       printf("%s: command not found\n", command);
     }
